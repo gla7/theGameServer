c8ca17113a8f950d2555ecf24c115abf027b8cf1
Gabriel Leon <gleon@recurly.com>
2017-12-10 23:47:01 -0700

    wrote a bunch of unit tests around authentication user and game interactions

--- test/tests.js
+++ test/tests.js
@@ -14,7 +14,7 @@ chai.use(chaiHttp)
 
 let token
 
-describe('All tests will now run', () => {
+describe('All tests:', () => {
   //Before starting the test, create a sandboxed database connection
   //Once a connection is established invoke done()
   before(done => {
@@ -27,7 +27,7 @@ describe('All tests will now run', () => {
     })
   })
 
-  describe("Homepage load", () => {
+  describe("Homepage tests:", () => {
   	it("Should be able to navigate to the homepage", done => {
   		chai.request(app)
   		.get("/")
@@ -39,15 +39,36 @@ describe('All tests will now run', () => {
   	})
   })
 
-  describe("Create user", () => {
+  describe("Authentication tests:", () => {
+    it("Should not mark me as logged in if I pass no token", done => {
+  		chai.request(app)
+  		.get("/loggedIn")
+  		.end((err, res) => {
+        err.response.text.should.equal('Unauthorized')
+  			res.should.have.status(401)
+  			done()
+  		})
+  	})
+
+    it("Should not mark me as logged in if I pass a bad token", done => {
+  		chai.request(app)
+  		.get("/loggedIn")
+      .set('Authorization', 'badToken')
+  		.end((err, res) => {
+        err.response.text.should.equal('Unauthorized')
+  			res.should.have.status(401)
+  			done()
+  		})
+  	})
+
     it("Should not be able to create user if name missing", done => {
   		chai.request(app)
   		.post("/createUser")
       .send({ email: 'test@test.com', password: 'test123' })
   		.end((err, res) => {
-        User.find({ email: 'test@test.com' }, (err, users) => {
-          if (err) { throw err }
-          should.equal(err, null)
+        User.find({ email: 'test@test.com' }, (errUser, users) => {
+          err.should.not.equal(null)
+          should.equal(errUser, null)
           users.length.should.equal(0)
     			res.should.have.status(422)
           done()
@@ -60,9 +81,9 @@ describe('All tests will now run', () => {
   		.post("/createUser")
       .send({ name: 'test', password: 'test123' })
   		.end((err, res) => {
-        User.find({ name: 'test' }, (err, users) => {
-          if (err) { throw err }
-          should.equal(err, null)
+        User.find({ name: 'test' }, (errUser, users) => {
+          err.should.not.equal(null)
+          should.equal(errUser, null)
           users.length.should.equal(0)
     			res.should.have.status(422)
           done()
@@ -75,9 +96,9 @@ describe('All tests will now run', () => {
   		.post("/createUser")
       .send({ name: 'test', email: 'test@test.com' })
   		.end((err, res) => {
-        User.find({ name: 'test' }, (err, users) => {
-          if (err) { throw err }
-          should.equal(err, null)
+        User.find({ name: 'test' }, (errUser, users) => {
+          err.should.not.equal(null)
+          should.equal(errUser, null)
           users.length.should.equal(0)
     			res.should.have.status(422)
           done()
@@ -90,9 +111,10 @@ describe('All tests will now run', () => {
   		.post("/createUser")
       .send({ name: 'test', email: 'test@test.com', password: 'test123' })
   		.end((err, res) => {
-        User.find({ name: 'test' }, (err, users) => {
+        User.find({ name: 'test' }, (errUser, users) => {
           if (err) { throw err }
-          should.equal(err, null)
+          should.equal(errUser, null)
+          users.length.should.equal(1)
     			res.should.have.status(200)
           token = res.body.token
           done()
@@ -105,9 +127,9 @@ describe('All tests will now run', () => {
   		.post("/createUser")
       .send({ name: 'test', email: 't@test.com', password: 'test123' })
   		.end((err, res) => {
-        User.find({ name: 'test' }, (err, users) => {
-          if (err) { throw err }
-          should.equal(err, null)
+        User.find({ name: 'test' }, (errUser, users) => {
+          err.should.not.equal(null)
+          should.equal(errUser, null)
           users.length.should.equal(1)
     			res.should.have.status(422)
           done()
@@ -120,19 +142,66 @@ describe('All tests will now run', () => {
   		.post("/createUser")
       .send({ name: 'tests', email: 'test@test.com', password: 'test123' })
   		.end((err, res) => {
-        User.find({ name: 'test' }, (err, users) => {
-          if (err) { throw err }
-          should.equal(err, null)
+        User.find({ name: 'test' }, (errUser, users) => {
+          err.should.not.equal(null)
+          should.equal(errUser, null)
           users.length.should.equal(1)
     			res.should.have.status(500)
           done()
         })
   		})
   	})
-  })
 
-  describe("Logged In", () => {
-  	it("Should mark me as logged in if I pass a token", done => {
+    it("Should mark me as logged in if I pass a token from sign up", done => {
+  		chai.request(app)
+  		.get("/loggedIn")
+      .set('Authorization', token)
+  		.end((err, res) => {
+  			should.equal(err, null)
+  			res.should.have.status(200)
+  			done()
+  		})
+  	})
+
+    it("Should not be able to sign in if passing wrong password", done => {
+  		chai.request(app)
+  		.post("/signIn")
+      .send({ name: 'test', password: 'test1234' })
+  		.end((err, res) => {
+        err.response.text.should.equal('Unauthorized')
+  			res.should.have.status(401)
+  			done()
+  		})
+  	})
+
+    it("Should not be able to sign in if passing wrong credentials", done => {
+  		chai.request(app)
+  		.post("/signIn")
+      .send({ name: 'tests', password: 'test123' })
+  		.end((err, res) => {
+        err.response.text.should.equal('Unauthorized')
+  			res.should.have.status(401)
+  			done()
+  		})
+  	})
+
+    it("Should be able to sign in if passing right credentials", done => {
+  		chai.request(app)
+  		.post("/signIn")
+      .send({ name: 'test', password: 'test123' })
+  		.end((err, res) => {
+        User.find({ name: 'test' }, (errUser, users) => {
+          if (err) { throw err }
+          should.equal(errUser, null)
+          users.length.should.equal(1)
+    			res.should.have.status(200)
+          token = res.body.token
+          done()
+        })
+  		})
+  	})
+
+    it("Should mark me as logged in if I pass a token from sign in", done => {
   		chai.request(app)
   		.get("/loggedIn")
       .set('Authorization', token)
@@ -144,7 +213,130 @@ describe('All tests will now run', () => {
   	})
   })
 
-  describe('Test Database', () => {
+  describe("Game controller tests:", () => {
+  	it("Should not be able to create game if not logged in", done => {
+  		chai.request(app)
+  		.post("/createGame")
+      .send({ name: 'gameTest' })
+  		.end((err, res) => {
+        Game.find({ name: 'gameTest' }, (errGame, games) => {
+          games.length.should.equal(0)
+          err.response.text.should.equal('Unauthorized')
+    			res.should.have.status(401)
+          done()
+        })
+  		})
+  	})
+
+    it("Should not be able to create game if sending a bad token", done => {
+  		chai.request(app)
+  		.post("/createGame")
+      .set('Authorization', 'badToken')
+      .send({ name: 'gameTest' })
+  		.end((err, res) => {
+        Game.find({ name: 'gameTest' }, (errGame, games) => {
+          games.length.should.equal(0)
+          err.response.text.should.equal('Unauthorized')
+    			res.should.have.status(401)
+          done()
+        })
+  		})
+  	})
+
+    it("Should not be able to create game if not sending at least a name", done => {
+  		chai.request(app)
+  		.post("/createGame")
+      .set('Authorization', token)
+      .send({ badName: 'gameTest' })
+  		.end((err, res) => {
+        Game.find({ name: 'gameTest' }, (errGame, games) => {
+          err.should.not.equal(null)
+          games.length.should.equal(0)
+    			res.should.have.status(500)
+          done()
+        })
+  		})
+  	})
+
+    it("Should be able to create game if sending at least a name whilst authenticated, and the user authenticated should be credited for it", done => {
+  		chai.request(app)
+  		.post("/createGame")
+      .set('Authorization', token)
+      .send({ name: 'gameTest' })
+  		.end((err, res) => {
+        User.find({ name: 'test' }, (errUser, users) => {
+          Game.findById(users[0].gamesCreated[0], (errGame, game) => {
+            game.author.toString().should.equal(users[0].id)
+      			res.should.have.status(200)
+            done()
+          })
+        })
+  		})
+  	})
+
+    it("Should not be able to create game if sending a taken name", done => {
+  		chai.request(app)
+  		.post("/createGame")
+      .set('Authorization', token)
+      .send({ name: 'gameTest' })
+  		.end((err, res) => {
+        Game.find({ name: 'gameTest' }, (errGame, games) => {
+          err.should.not.equal(null)
+          res.body.error.should.equal('This game name already exists!')
+          games.length.should.equal(1)
+    			res.should.have.status(422)
+          done()
+        })
+  		})
+  	})
+
+    it("Should not be able to destroy game if sending a non-existent name", done => {
+  		chai.request(app)
+  		.get("/destroyGame/nonExistentGameName")
+      .set('Authorization', token)
+  		.end((err, res) => {
+        Game.find({ name: 'gameTest' }, (errGame, games) => {
+          should.equal(err, null)
+          games.length.should.equal(1)
+    			res.should.have.status(200)
+          done()
+        })
+  		})
+  	})
+
+    it("Should not be able to destroy game if sending a bad token", done => {
+  		chai.request(app)
+  		.get("/destroyGame/gameTest")
+      .set('Authorization', 'badToken')
+  		.end((err, res) => {
+        Game.find({ name: 'gameTest' }, (errGame, games) => {
+          games.length.should.equal(1)
+          err.response.text.should.equal('Unauthorized')
+    			res.should.have.status(401)
+          done()
+        })
+  		})
+  	})
+
+    it("Should be able to destroy game if the right user is authenticated, and this should be reflected in the author", done => {
+  		chai.request(app)
+  		.get("/destroyGame/gameTest")
+      .set('Authorization', token)
+  		.end((err, res) => {
+        User.find({ name: 'test' }, (errUser, users) => {
+          Game.find({ name: 'gameTest' }, (errGame, games) => {
+            users[0].gamesCreated.length.should.equal(0)
+            games.length.should.equal(0)
+      			res.should.have.status(200)
+            res.text.should.equal('Success')
+            done()
+          })
+        })
+  		})
+  	})
+  })
+
+  describe('DB tests:', () => {
     //Save object with 'name' value of 'Mike"
     it('New name saved to test database', done => {
       var testName = new Game({
